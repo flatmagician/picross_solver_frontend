1f6f2da2ea597317bae1b1b3721617671d73c26d
Ben Taussig <bpt28@cornell.edu>
2020-01-03 12:48:50 -0500

    Updates

---
+++ precache-manifest.59f159c458352101d617bb2a369610c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0213fb15cc05b8182b8cebd43772cc7e",
+    "url": "/picross_solver_frontend/index.html"
+  },
+  {
+    "revision": "eb26c68ff211c64ddde3",
+    "url": "/picross_solver_frontend/static/css/main.de0ba8ba.chunk.css"
+  },
+  {
+    "revision": "03e1a8e3a6856367c113",
+    "url": "/picross_solver_frontend/static/js/2.9c0d34f1.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/picross_solver_frontend/static/js/2.9c0d34f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb26c68ff211c64ddde3",
+    "url": "/picross_solver_frontend/static/js/main.5ae8529c.chunk.js"
+  },
+  {
+    "revision": "01a3d8e99834679e0339",
+    "url": "/picross_solver_frontend/static/js/runtime-main.0e9468ed.js"
+  }
+]);
