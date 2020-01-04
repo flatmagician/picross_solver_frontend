0782236ccf59bab3eaeafd1c7dc10fbd0eb4de7d
Ben Taussig <bpt28@cornell.edu>
2020-01-04 01:16:03 -0500

    Updates

--- precache-manifest.00c99f94a546051f7534b3aec1a166ef.js
+++ precache-manifest.00c99f94a546051f7534b3aec1a166ef.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1455f682446aea0d9e4288ba3d5b8a6a",
+    "revision": "8463aac46d53353e0dd2a0eaae2a412b",
     "url": "/picross_solver_frontend/index.html"
   },
   {
-    "revision": "31473a9c73c2ae632030",
-    "url": "/picross_solver_frontend/static/css/main.ea15cb9b.chunk.css"
+    "revision": "0c37b56efabbc3b5d09a",
+    "url": "/picross_solver_frontend/static/css/main.b5b165ed.chunk.css"
   },
   {
     "revision": "03e1a8e3a6856367c113",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/picross_solver_frontend/static/js/2.9c0d34f1.chunk.js.LICENSE"
   },
   {
-    "revision": "31473a9c73c2ae632030",
-    "url": "/picross_solver_frontend/static/js/main.fb82d4ff.chunk.js"
+    "revision": "0c37b56efabbc3b5d09a",
+    "url": "/picross_solver_frontend/static/js/main.5a8a8dbc.chunk.js"
   },
   {
     "revision": "01a3d8e99834679e0339",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/picross_solver_frontend/precache-manifest.62f943b9e36ba357c24370344d700951.js"
+  "/picross_solver_frontend/precache-manifest.00c99f94a546051f7534b3aec1a166ef.js"
 );
 
 self.addEventListener('message', (event) => {
